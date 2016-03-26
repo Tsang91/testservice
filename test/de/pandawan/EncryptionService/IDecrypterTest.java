1dbdbc5da96d546640346308e332d7674908b3c8
vuong <vuongngo@outlook.de>
2016-03-26 14:34:30 +0100

    add test

--- test/de/pandawan/EncryptionService/IDecrypterTest.java
+++ test/de/pandawan/EncryptionService/IDecrypterTest.java
@@ -1,5 +1,6 @@
 package de.pandawan.EncryptionService;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -8,7 +9,20 @@ import org.junit.Test;
 public class IDecrypterTest {
 
     @Test
-    public void decrypt() throws Exception {
+    public void testDecrypt() throws Exception {
+        //arrange
+        String stringKey = "Testkey!TestKey!";
+        String result = "Test!";
+        byte[] encrypted = new byte[]{ -122, -36, 126, -91, -125, -121, -23, 64, 52, -92, -22, 121, -7, 79, 22, 111};
 
+        CustomKey key = new CustomKey();
+        key.setKeyArray(stringKey.getBytes());
+        IDecrypter decrypter = new DecrypterImpl(key);
+
+        //act
+        String output = decrypter.decrypt(encrypted);
+
+        //assert
+        Assert.assertTrue(String.format("String should be %1$s", result), output.matches(result));
     }
 }
