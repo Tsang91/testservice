2f3b1e30c7b0eddfe6aedc709bc27d08ccc45b71
vuong <vuongngo@outlook.de>
2016-03-27 23:23:17 +0200

    refactoring

--- test/de/pandawan/MySqlDatabaseService/ISqlQueryCreatorTest.java
+++ test/de/pandawan/MySqlDatabaseService/ISqlQueryCreatorTest.java
@@ -3,6 +3,9 @@ package de.pandawan.MySqlDatabaseService;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 /**
  * Created by vuong on 27.03.16.
  */
@@ -19,6 +22,30 @@ public class ISqlQueryCreatorTest {
 
         //assert
         Assert.assertTrue("QueryObject shouldn't be null", contextItem != null);
+    }
+
+    @Test
+    public void testQueryReturnValueIsNotNull() throws Exception{
+        //arrange
+        String query = new String("select * from passwordTable");
+        ISqlQueryCreator queryCreator = new SqlQueryCreator();
+        QueryContextItem contextItem = queryCreator.createQueryContextItem(query);
+        Connection connection = new SqlConnectionFactory(
+            new LoginToken(
+                "jdbc:mysql://localhost:32768/PasswordDB",
+                "root",
+                "root"
+                ),
+            "MySql"
+        ).establishConnection();
+
+        //act
+        ResultSet set = connection.createStatement()
+            .executeQuery(queryCreator.createQueryContextItem(query).getQuery());
+        set.close();
+
+        //assert
+        Assert.assertTrue("ResultSet shouldnt be null", set != null);
     }
 
 }
