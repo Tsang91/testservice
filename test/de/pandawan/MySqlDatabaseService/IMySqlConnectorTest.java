b4eb452be4f8aa12ed8089830b689378626d17fb
vuong <vuongngo@outlook.de>
2016-03-27 16:29:52 +0200

    add statement test

--- test/de/pandawan/MySqlDatabaseService/IMySqlConnectorTest.java
+++ test/de/pandawan/MySqlDatabaseService/IMySqlConnectorTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.Statement;
 
 /**
  * Created by vuong on 27.03.16.
@@ -23,5 +24,20 @@ public class IMySqlConnectorTest {
 
         //assert
         Assert.assertTrue("Connection shouldn't be null", connection != null);
+    }
+
+    @Test
+    public void testCreateStatement() throws Exception{
+        //arrange
+        String url = "jdbc:mysql://localhost:32768";
+        String user = "root";
+        String password = "root";
+        Connection connection = new MySqlConnectorImpl(url, user, password).connecionToDatabase();
+
+        //act
+        Statement statement = connection.createStatement();
+
+        //assert
+        Assert.assertTrue("Statement shoudln't be null", statement != null);
     }
 }
